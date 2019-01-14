4fe5983bfda582ca3d3663abe9f9ed81cff45a87
poymanov <n.poymanov@gmail.com>
2019-01-14 20:37:23 +0300

    Init project

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {   
+    componentWillUpdate() {
+        
+    }
+
+    render () {
+        const ingredientsSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>
+                        {igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>
+        });
+
+        return (
+            <>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </>
+        );
+    }    
+};
+
+export default OrderSummary;
